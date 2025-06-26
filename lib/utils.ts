6af09c4bff26609a42ca2f33d12e3c1fe97c6f3c
fanglanyan <fanglanyan@info.easeus.com.cn>
2025-06-26 09:58:25 +0800

    test

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// This check can be removed, it is just for tutorial purposes
+// This check can be removed, it is just for tutorial purposes 我就想看看是哪里不对
 export const hasEnvVars =
   process.env.NEXT_PUBLIC_SUPABASE_URL &&
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
